72803f6e23feb99bc3d7d0f40a0f9a06786fe5a3
Rasreal <ersultan.t16@gmail.com>
2025-04-08 19:24:43 +0500

    feat: fallingText added

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FallingText from "./animation/FallingText.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+      {/*<div>*/}
+      {/*  <a href="https://vite.dev" target="_blank">*/}
+      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
+      {/*  </a>*/}
+      {/*  <a href="https://react.dev" target="_blank">*/}
+      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
+      {/*  </a>*/}
+      {/*</div>*/}
+      {/*<h1>Vite + React</h1>*/}
+
+        <div className="container">
+            <h2>Оң жағыңызда ҚұлайтынТекст анимациясы, міндетті түрде враппер контейнер мен height беру керек.</h2>
+            <FallingText
+                text={`Дәл сол анимацияны осы жерден көре аласыз `}
+                highlightWords={["анимацияны", "көре", "аласыз", "жерден", "сол"]}
+                highlightClass="highlighted"
+                trigger="hover"
+                backgroundColor="transparent"
+                wireframes={false}
+                gravity={0.56}
+                fontSize="2rem"
+                mouseConstraintStiffness={0.9}
+            />
+        </div>
+
+
+    </div>
   )
 }
 
