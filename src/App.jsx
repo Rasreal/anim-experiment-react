baf0f569a3052f0da7943dacd2c292e8d2443826
Rasreal <ersultan.t16@gmail.com>
2025-04-12 23:45:05 +0500

    feat: fallbackk text made dynamic (demo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useLayoutEffect, useRef, useState} from 'react';
+import './App.css';
 import FallingText from "./animation/FallingText.jsx";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
+    const [inputValue, setInputValue] = useState("Дәл сол анимацияны осы жерден көре аласыз");
+    const inputRef = useRef();
 
-  return (
-    <div className="page">
-      {/*<div>*/}
-      {/*  <a href="https://vite.dev" target="_blank">*/}
-      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
-      {/*  </a>*/}
-      {/*  <a href="https://react.dev" target="_blank">*/}
-      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
-        <div className="input-container">
+    useLayoutEffect(() => {
+        inputRef.current.focus();
+
+    })
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+        localStorage.setItem('inputValue', inputValue);
+
+    };
+
+    useEffect(() => {
+        const savedValue = localStorage.getItem('inputValue');
+        if (savedValue) {
+            setInputValue(savedValue);
+        }
+    }, []);
 
 
+    return (
+        <div className="page">
+            <div className="input-container">
+                <textarea
+                    ref={inputRef}
+                    rows={5}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div className="container">
+                <div>
+                    <h2>Оң жағыңызда ҚұлайтынТекст анимациясы, міндетті түрде <strong>враппер
+                        контейнер</strong> мен <b>height</b> беру керек.</h2>
+                    <div className="reminder-text">
+                        <span className="fa fa-exclamation" style={{marginRight: "10px"}}></span>
+                        <p>Текст құлауы үшін, үстінен басыңыз</p>
+                    </div>
+                    <div className="reminder-text">
+                        <span className="fa fa-bell" style={{marginRight: "10px"}}></span>
+                        <p>Жүйе құлаған соң, парақты қайта <strong>жүктеу</strong> керек!</p>
+                    </div>
+
+                </div>
+
+
+                <FallingText
+                    text={`${inputValue || ""}`}
+                    highlightWords={["анимацияны", "көре", "аласыз", "жерден", "сол"]}
+                    highlightClass="highlighted"
+                    trigger="click"
+                    backgroundColor="transparent"
+                    wireframes={false}
+                    gravity={0.56}
+                    fontSize="2rem"
+                    mouseConstraintStiffness={0.9}
+                />
+
+            </div>
         </div>
-        <div className="container">
-            {/*Original component-те құлап қалған текст, сыртқа ұшып кете алады*/}
-            <h2>Оң жағыңызда ҚұлайтынТекст анимациясы, міндетті түрде <strong>враппер контейнер</strong> мен <b>height</b> беру керек.</h2>
-            <FallingText
-                text={`Дәл сол анимацияны осы жерден көре аласыз `}
-                highlightWords={["анимацияны", "көре", "аласыз", "жерден", "сол"]}
-                highlightClass="highlighted"
-                trigger="hover"
-                backgroundColor="transparent"
-                wireframes={false}
-                gravity={0.56}
-                fontSize="2rem"
-                mouseConstraintStiffness={0.9}
-            />
-        </div>
-
-
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Input () {
+    return (
+        <React.Fragment>
+            <input>
+
+            </input>
+        </React.Fragment>
+
+    )
+}
